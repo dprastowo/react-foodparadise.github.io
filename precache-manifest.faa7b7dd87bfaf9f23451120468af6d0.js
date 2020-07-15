ff7a6e52383d3fdd5b87aff3fa3a5e7c8e8b7e80
Dwi Prastowo <dprastowo@gmail.com>
2020-07-16 03:17:18 +0700

    deploy foodparadise

---
+++ precache-manifest.faa7b7dd87bfaf9f23451120468af6d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dad21bbb8f2858c8f38fe7e63ec65be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6478e5debf34bcc6186",
+    "url": "/static/css/main.a6ae1d33.chunk.css"
+  },
+  {
+    "revision": "7f0ce6114347f0de3553",
+    "url": "/static/js/2.ac14195d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac14195d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6478e5debf34bcc6186",
+    "url": "/static/js/main.6df3794a.chunk.js"
+  },
+  {
+    "revision": "28648d4b86d330626cee",
+    "url": "/static/js/runtime-main.fafa68b7.js"
+  },
+  {
+    "revision": "00031694b823bfa49a63afb1b5de0edf",
+    "url": "/static/media/mie-ayam-bakso.00031694.jpg"
+  }
+]);
